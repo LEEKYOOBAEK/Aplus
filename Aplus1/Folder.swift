d71601507f445052c4a87d7af5b674df1ae75831
sm <lsm980130@gmail.com>
2018-08-04 20:23:52 +0900

    Revert "AudioPlay1"
    This reverts commit 725f598d88b6077cd1edb6f864aef294d66ded83.

---
+++ Aplus1/Folder.swift
@@ -0,0 +1,22 @@
+//
+//  folder.swift
+//  Aplus
+//
+//  Created by CAUAD06 on 2018. 8. 1..
+//  Copyright © 2018년 CAUAD06. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Folder {
+    var name:String?
+
+    init(name:String?) {
+        self.name = name
+    }
+
+
+}
+
+
