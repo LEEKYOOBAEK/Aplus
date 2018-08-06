21e1e7f83900c78be7c07f02a2db9e76cd5ba154
AhnYeLim <ayl5033@gmail.com>
2018-08-06 17:26:14 +0900

    this is my revolution!
    make new folder by alert.

---
+++ finalTableView/DetailTableViewController.swift
@@ -0,0 +1,134 @@
+//
+//  DetailTableViewController.swift
+//  finalTableView
+//
+//  Created by CAUAD05 on 2018. 7. 30..
+//  Copyright © 2018년 ahnYeLim. All rights reserved.
+//
+
+import UIKit
+
+class DetailTableViewController: UITableViewController {
+    
+    
+     var selectedFile:RecordFile?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+//        guard let selectedFile = selectedFile else {
+//            return
+//        }
+        
+        
+       // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+//    var delegate:MainTableViewController
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 5
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        
+
+        guard let myCell = cell as? MyTableViewCell2 else{
+        // Configure the cell...
+
+        return cell
+    }
+        
+        myCell.mainName.text = selectedFile?.name
+        myCell.subTitle.text = selectedFile?.subTitle
+        self.navigationItem.title = selectedFile?.name
+        
+        return cell
+    }
+    
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+}
+
+    class MyTableViewCell2: UITableViewCell {
+    
+    @IBOutlet weak var mainName: UILabel!
+    @IBOutlet weak var subTitle: UILabel!
+    @IBOutlet weak var date: UILabel!
+    @IBOutlet weak var recordLength: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization cod}
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        // Configure the view for the selected state
+    }
+    
+}
+
