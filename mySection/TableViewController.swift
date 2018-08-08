e5b7cd1bfd0c35105e3484fd65999cc92d9cde15
AhnYeLim <ayl5033@gmail.com>
2018-08-08 20:19:58 +0900

    AddFolder

--- mySection/TableViewController.swift
+++ mySection/TableViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 
+
 class TableViewController: UITableViewController {
     
     struct Section {
@@ -15,13 +16,13 @@ class TableViewController: UITableViewController {
         var records:[RecordFile]
     }
     
-    var sectionArray:[Section] = []
+    var sectionArray:[Section] = [Section(sectionName: "폴더", records: []),Section(sectionName: "음성녹음", records: [])]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        let record1 = RecordFile(fileName: "한국사", fileSubtitle: nil, fileDate: 0, fileLength: 0)
-        let record2 = RecordFile(fileName: "음성녹음1", fileSubtitle: nil, fileDate: 0, fileLength: 0)
-        sectionArray = [Section(sectionName: "폴더", records: [record1]),Section(sectionName: "음성파일", records: [record2])]
+//        let record1 = RecordFile(fileName: "한국사", fileSubtitle: nil, fileDate: 0, fileLength: 0)
+//        let record2 = RecordFile(fileName: "음성녹음1", fileSubtitle: nil, fileDate: 0, fileLength: 0)
+//        sectionArray = [Section(sectionName: "폴더", records: [record1]),Section(sectionName: "음성파일", records: [record2])]
 
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
@@ -34,8 +35,23 @@ class TableViewController: UITableViewController {
         let alert = UIAlertController(title: "폴더 생성", message: "폴더의 이름을 넣어주세요", preferredStyle: .alert)
         
         alert.addTextField { (textField) in
-            textField.placeholder = "폴"
+            textField.placeholder = "폴더 이름"
         }
+       
+        let okAction = UIAlertAction(title: "OK", style: .default) { (action) in
+            if alert.textFields?[0].text == "" {
+                let record1 = RecordFile(fileName: "음성녹음\(self.sectionArray[0].records.count + 1)", fileSubtitle: nil, fileDate: 0, fileLength: 0)
+                self.sectionArray[0].records.append(record1)
+            }else {
+                let record1 = RecordFile(fileName: (alert.textFields?[0].text)!, fileSubtitle: nil, fileDate: 0, fileLength: 0)
+                self.sectionArray[0].records.append(record1)
+            }
+            self.tableView.reloadData()
+        }
+        alert.addAction(okAction)
+        present(alert, animated: true, completion:nil)
+    
+    
     }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
