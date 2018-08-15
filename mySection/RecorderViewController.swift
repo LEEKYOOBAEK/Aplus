e41ac014a26386d6618517e0608da13539ddfe6d
AhnYeLim <ayl5033@gmail.com>
2018-08-16 00:42:24 +0900

    Revert "icon"
    This reverts commit e0c11dbef6695b7bbdeccd79ba6f2fe2ad577d22.

--- mySection/RecorderViewController.swift
+++ mySection/RecorderViewController.swift
@@ -59,19 +59,68 @@ class RecorderViewController: UIViewController, AVAudioRecorderDelegate {
     @IBAction func save(_ sender: Any) {
         numberOfRecords += 1
     
-        self.audioRecorder.stop()
-        self.audioRecorder = nil
+        //alert로 저장 or 삭제
+        let alert = UIAlertController(title: "음성 파일을 저장할까요?", message: "", preferredStyle: .alert)          //alert 형식으로 나타나도록 + 메시지
+        
+        
+        //alert에서 '저장' 클릭하면 저장 후 파일 목록화면으로
+        let saveAction = UIAlertAction(title: "저장", style:.default)
+        { (action) in
             
-        self.progressTimer.invalidate()
+            //stop audio recording
+            self.audioRecorder.stop()        //녹음 멈춤
+            self.audioRecorder = nil         //녹음 종료
             
-        UserDefaults.standard.set(numberOfRecords, forKey: "myRecordFileNumber")
-        self.dismiss(animated: true, completion: nil)
+            self.progressTimer.invalidate()      //녹음 저장하면 타이머 무효화
             
+            UserDefaults.standard.set(numberOfRecords, forKey: "myRecordFileNumber")
+            self.dismiss(animated: true, completion: nil)
+            
+            //새로운 recording을 얻었기 때문
+            self.dismiss(animated: true, completion: nil)       //모달로 연결 했을 때 이전 화면으로 돌아가기
         }
         
         
         
-    
+        //alert에서 '삭제' 클릭하면 파일 삭제
+        let deleteAction = UIAlertAction(title: "삭제", style: .default)
+        { (action) in
+            //삭제 다시 물어보는 alert로
+            let alert2 = UIAlertController(title: "녹음 삭제", message: "음성파일\(numberOfRecords)을(를) 삭제 하겠습니까?", preferredStyle: .alert)
+            
+            let cancelAction = UIAlertAction(title: "취소", style: .default)  //창닫기
+            let realdelAction = UIAlertAction(title: "삭제", style: .default)
+            { (action) in
+                do {
+                    try FileManager.default.removeItem(at:self.selectedFilePath.appendingPathComponent("\(numberOfRecords).m4a"))
+                    print("삭제할 파일은\(self.selectedFilePath.appendingPathComponent("\(numberOfRecords).m4a"))")
+                }catch{
+                    print("cannot delete")
+                    return
+                }
+
+                self.dismiss(animated: true, completion: nil)
+                
+            }
+            alert2.addAction(cancelAction)
+            alert2.addAction(realdelAction)
+            self.present(alert2, animated: true, completion: nil)
+            
+            //            let alert2 = UIAlertController(title: "녹음 삭제", message: "\(self.alert.TextField?[0].text)을(를) 삭제 하겠습니까?", preferredStyle: .alert)
+        }
+        
+        alert.addAction(saveAction)     //'저장'액션 추가
+        alert.addAction(deleteAction)   //'삭제'액션 추가
+        self.present(alert, animated: true, completion: nil)    //alert 나타나도록
+        
+        
+        self.audioRecorder.pause()      //'삭제'누르면 녹음 중지
+        self.startBtn.setTitle("Start Recording", for: .normal)   //버튼이름 바뀜
+        
+        //
+        
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         
